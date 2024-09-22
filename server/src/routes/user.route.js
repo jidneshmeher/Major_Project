c06fcb84cd36043d944bf0cd91efa9edef56effd
Jidnesh Meher <meherjidnesh89@gmail.com>
2024-09-22 23:30:26 +0530

    Add Logout Fetaure

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
-import { signUp } from "../controllers/user.controller.js";
+import { logout, signUp } from "../controllers/user.controller.js";
 import { signIn } from "../controllers/user.controller.js";
+import { verifyToken } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
 router.post('/sign-up',signUp)
-
 router.post('/sign-in',signIn)
+
+// Secured Routes
+router.post('/logout',verifyToken,logout)
 
 
 export default router
